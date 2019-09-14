64c2be04932f46b6eb4715dfdc944945d82d1a8b
lindyhopfan <josh.sarean@gmail.com>
2019-09-13 17:06:57 -0700

    back to prod

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -1,6 +1,6 @@
 var config = {
   paths: {
-    "paystand": "https://checkout.paystand.co/v4/js/paystand.checkout",
+    "paystand": "https://checkout.paystand.com/v4/js/paystand.checkout",
     "paystand-sandbox": "https://checkout.paystand.co/v4/js/paystand.checkout",
     "paystand-checkout": "./PayStand_PayStandMagento/js/view/checkout/checkout"
   }
