8e138cf37fb5b390b1c065d05eb7d459497195d9
lindyhopfan <josh.sarean@gmail.com>
2019-09-17 17:20:12 -0700

    rename prod module

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -1,6 +1,6 @@
 var config = {
   paths: {
-    "paystand": "https://checkout.paystand.com/v4/js/paystand.checkout",
+    "paystand-prod": "https://checkout.paystand.com/v4/js/paystand.checkout",
     "paystand-sandbox": "https://checkout.paystand.co/v4/js/paystand.checkout",
     "paystand-checkout": "./PayStand_PayStandMagento/js/view/checkout/checkout"
   }
