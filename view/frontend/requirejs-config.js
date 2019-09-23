0ed3e06166d9449352dec2a6da1a538c2b8c57e3
lindyhopfan <josh.sarean@gmail.com>
2019-09-23 09:37:36 -0700

    temporary local env

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -1,7 +1,8 @@
 var config = {
   paths: {
-    "paystand": "https://checkout.paystand.com/v4/js/paystand.checkout",
-    "paystand-sandbox": "https://checkout.paystand.co/v4/js/paystand.checkout",
+    "paystand": "https://checkout.paystand.com/v4/js/paystand.checkout?env=live",
+    //"paystand-sandbox": "https://checkout.paystand.co/v4/js/paystand.checkout?env=sandbox",
+    "paystand-sandbox": "https://localhost:3002/js/paystand.checkout.js?env=local",
     "paystand-checkout": "./PayStand_PayStandMagento/js/view/checkout/checkout"
   }
 };
