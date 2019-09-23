3d212d9636dc5af43889bd3c7ea527a6d4b7b044
lindyhopfan <josh.sarean@gmail.com>
2019-09-23 12:53:28 -0700

    fix blank checkout issue

--- view/frontend/web/js/view/checkout/checkout.js
+++ view/frontend/web/js/view/checkout/checkout.js
@@ -5,12 +5,12 @@ var checkout_domain = 'checkout.paystand.com';
 var use_sandbox = window.checkoutConfig.payment.paystandmagento.use_sandbox;
 if (use_sandbox == '1') {
   checkoutjs_module = 'paystand-sandbox';
-  // core_domain = 'paystand.co';
-  // api_domain = 'api.paystand.co';
-  // checkout_domain = 'checkout.paystand.co';
-  core_domain = 'localhost:3001';
-  api_domain = 'localhost:3001/api';
-  checkout_domain = 'localhost:3002';
+  core_domain = 'paystand.co';
+  api_domain = 'api.paystand.co';
+  checkout_domain = 'checkout.paystand.co';
+  // core_domain = 'localhost:3001';
+  // api_domain = 'localhost:3001/api';
+  // checkout_domain = 'localhost:3002';
 }
 
 /*jshint browser:true jquery:true*/
