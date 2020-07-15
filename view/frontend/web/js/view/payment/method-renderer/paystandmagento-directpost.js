dbb6ca008e07b302a7c432775eb242179022c116
Jose Manuel Beltran Loreto <jm.beltran.loreto@gmail.com>
2020-07-15 14:57:53 -0500

    point to biz

--- view/frontend/web/js/view/payment/method-renderer/paystandmagento-directpost.js
+++ view/frontend/web/js/view/payment/method-renderer/paystandmagento-directpost.js
@@ -5,9 +5,9 @@ var checkout_domain = 'checkout.paystand.com';
 var use_sandbox = window.checkoutConfig.payment.paystandmagento.use_sandbox;
 if (use_sandbox == '1') {
     checkoutjs_module = 'paystand-sandbox';
-    core_domain = 'paystand.co';
-    api_domain = 'api.paystand.co';
-    checkout_domain = 'checkout.paystand.co';
+    core_domain = 'paystand.biz';
+    api_domain = 'api.paystand.biz';
+    checkout_domain = 'checkout.paystand.biz';
 }
 
 define(
