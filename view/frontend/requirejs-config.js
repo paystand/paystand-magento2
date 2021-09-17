1c45e6e0a754c2ffb79e5953d7dfbe74330ad8ea
Jared Heck <jared@admins-MacBook-Pro.local>
2021-09-17 14:33:54 -0700

    revert config changes

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -1,7 +1,7 @@
 var config = {
   paths: {
     "paystand": "https://checkout.paystand.com/v4/js/paystand.checkout.js?env=live",
-    "paystand-sandbox": "https://localhost:3003/v4/js/paystand.checkout.js?env=local",
+    "paystand-sandbox": "https://checkout.paystand.co/v4/js/paystand.checkout.js?env=sandbox",
     //"paystand-sandbox": "https://checkout.paystand.co/v4/js/paystand.checkout.js?env=local_dev",
   }
 };
