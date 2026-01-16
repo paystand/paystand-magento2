0c83ab694c3380b3e5325b6fac67e859e2ac9632
Alejandro Velazquez <avelazquez@paystand.com>
2026-01-16 15:40:28 -0600

    changing env to sandbox

--- view/frontend/web/js/view/payment/method-renderer/paystandmagento-directpost.js
+++ view/frontend/web/js/view/payment/method-renderer/paystandmagento-directpost.js
@@ -6,10 +6,10 @@ var env = 'live';
 var use_sandbox = window.checkoutConfig.payment.paystandmagento.use_sandbox;
 if (use_sandbox == '1') {
     checkoutjs_module = 'paystand-sandbox';
-    core_domain = 'paystand.biz';
-    api_domain = 'api.paystand.biz';
-    checkout_domain = 'checkout.paystand.biz';
-    env = 'development'
+    core_domain = 'paystand.co';
+    api_domain = 'api.paystand.co';
+    checkout_domain = 'checkout.paystand.co';
+    env = 'sandbox'
 }
 
 define(
